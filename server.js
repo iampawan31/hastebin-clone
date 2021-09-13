1aa3bc8951818237ee3a99b686914fa84a271810
Pawan Kumar <surpawan@gmail.com>
2021-09-13 19:04:34 +0530

    Added Duplicate logic

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const mongoose = require("mongoose");
+const Document = require("./models/Document");
+
+mongoose.connect(process.env.MONGO_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 app.get("/", (req, res) => {
   const code = `Welcome to Hastebin!
@@ -9,7 +20,54 @@ app.get("/", (req, res) => {
 Use the commands in the top right corner
 to create a new file to share with others`;
 
-  res.render("code-display", { title: "Hastebin Clone", code });
+  res.render("code-display", {
+    language: "plaintext",
+    code,
+  });
+});
+
+app.get("/new", (req, res) => {
+  res.render("new", { title: "New" });
+});
+
+app.get("/:id/duplicate", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const { value } = await Document.findById(id);
+
+    res.render("new", {
+      value: value,
+    });
+  } catch (error) {
+    res.redirect(`/${id}`);
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const { value } = await Document.findById(id);
+
+    res.render("code-display", {
+      code: value,
+      id,
+    });
+  } catch (error) {
+    res.redirect("/");
+  }
+});
+
+app.post("/save", async (req, res) => {
+  const value = req.body.value;
+
+  try {
+    const document = await Document.create({ value });
+    res.redirect(`/${document.id}`);
+  } catch (error) {
+    res.render("new", { value });
+  }
 });
 
 app.listen(3000);
